7c77e5b1bfb9bd7ef98ba0ae1c5b6d4348069c0f
AlFathahGans <alfathah55@gmail.com>
2022-09-17 21:27:47 +0700

    BaruBelajar

---
+++ PesawatTelpon.java
@@ -0,0 +1,11 @@
+public class PesawatTelpon extends Hewan{	
+
+	String bentuk, kabel, gagang_telepon; 	
+
+	String terima_panggilan(String t){		
+		return t;	
+	} 	
+	String melakukan_panggilan(String m){		
+		return m;	
+	}
+}
